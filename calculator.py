103c30fac15ab7794cbf0ac9b3b43cc4cb6cbe1c
katakamX <uni.katakam@gmail.com>
2025-01-25 14:09:17 +0530

    second commit

--- calculator.py
+++ calculator.py
@@ -16,6 +16,62 @@ def get_numbers():
             
     return numbers
         
-get_numbers()
+def calculator():
+
+    print("Calculator Prog" )
+    print("enter 1 for Addition")
+    print("enter 2 for substraction")
+    print("enter 3 for division")
+    print("enter 4 for multiplication")
+    
+    while True:
+        choice = input("enter the option")
+        
+        if choice in ['1', '2', '3', '4']:
+            numbers = get_numbers()
+            
+            if len(numbers) < 2:
+                print("enter atleast 2 numbers")
+                
+                continue
+            if choice == '1':
+                result =  sum(numbers)
+                print("the result is: " , result)
+                            
+            elif choice == '2':
+                result = numbers[0]
+                for num in numbers[1:]:
+                    result = result-num
+                    print("the result is :", result)
+                    
+            elif choice == '3':
+                result = numbers[0]
+                for num in numbers[1:]:
+                    result = result * num
+                    print("the result is:", result)
+                    
+            elif choice == '4':
+                result = numbers[0]
+                for num in numbers[1:]:
+                    result = result / num
+                    print("result is: ", result)
+                    
+        else: 
+            print("invalid response")
+            
+        next_calc = input("do you wish to perform another calculation? type yes/no")
+             
+        if next_calc == 'yes':
+            calculator()
+            
+        else:
+            print("goodbye")
+            break
+        
+
+calculator()
+            
+                
+                       
     
         
