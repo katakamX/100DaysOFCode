477e2742f1d729d03c23e48d85edf846adab7e49
katakamX <uni.katakam@gmail.com>
2025-01-30 21:29:22 +0530

    .

---
+++ strings.py
@@ -0,0 +1,18 @@
+# print('he wants to eat "an apple" ')
+
+# a = '''hello nigga
+        # you haev skill issue migga
+        
+        
+            # .'''
+# print(a)
+
+# print(type(a)) \   
+
+name = "nin hesaru nigga"
+i = 1
+for n in name:
+   
+    print(name[0:i])
+    i = i + 1
+    
