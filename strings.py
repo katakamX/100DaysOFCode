6648612fb237fb6364b8740ef1b32963ab354195
katakamX <uni.katakam@gmail.com>
2025-02-10 09:12:58 +0530

    .

--- strings.py
+++ strings.py
@@ -19,6 +19,41 @@
 
 a = "aditya katakam"
 
-print(a[0:4])
-print(a[1:8])
-   
+# print(a[0:4]) #including 0 but not including 4
+# print(a[1:8]) # including 1 but not including 8
+
+# print(a[0:-3])
+
+
+'''reversed_char = " "
+for n in reversed(a):
+    reversed_char = reversed_char + n
+    
+print(reversed_char)'''
+
+
+
+'''b = "kattu"
+c = "1234567890"
+reversed_cchar = ""
+for n in reversed(c):
+    reversed_cchar = reversed_cchar + n
+    
+print(reversed_cchar) '''  
+
+ 
+# snippet for reversing numbers by converting it to a string:
+list= [1,2,3,4]
+strring = ""
+for d in reversed(list):
+    strring = strring + str(d)
+print(int(strring))
+      
+      
+#  same but not reversed
+
+list= [1,2,3,4]
+strring = ""
+for d in list:
+    strring = strring + str(d)
+print(int(strring))
