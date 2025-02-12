334ac08543ac8262885493591a73abcd22836a92
katakamX <uni.katakam@gmail.com>
2025-02-12 21:12:30 +0530

    .

--- stringmeth.py
+++ stringmeth.py
@@ -1,4 +1,8 @@
-a = "aditya"
+a = "adititytia!!!!!!!!!"
 print(len(a))
 print(a.upper())
 print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("aditya", "nigga"))
+print(a.split("t",maxsplit=3))
+print(a.capitalize())
