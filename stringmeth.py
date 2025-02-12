b4a8d90adf72ba9540028b08fc33c48c17f2e28f
katakamX <uni.katakam@gmail.com>
2025-02-12 21:28:28 +0530

    .

--- stringmeth.py
+++ stringmeth.py
@@ -1,4 +1,5 @@
 a = "adititytia!!!!!!!!!"
+b = "diddy party awasd aw sd awsdf sf sef sdsdf a sdf s sdf eaf sdf sf"
 print(len(a))
 print(a.upper())
 print(a.lower())
@@ -6,3 +7,7 @@ print(a.rstrip("!"))
 print(a.replace("aditya", "nigga"))
 print(a.split("t",maxsplit=3))
 print(a.capitalize())
+print(a.center(50))
+print(b.count("sf"))
+print(a.endswith("!"))
+
