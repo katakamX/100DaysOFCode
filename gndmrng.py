0a9f5982a67ebcc563e246ee97f5f5c351023fe2
katakamX <uni.katakam@gmail.com>
2025-02-13 11:17:29 +0530

    .

--- gndmrng.py
+++ gndmrng.py
@@ -1,11 +1,11 @@
 import time
 timestamp = time.strftime('%H:%M:%S')
 print(timestamp)
-timestamp = int(time.strftime('%H'))
+timestamph = int(time.strftime('%H'))
 print(timestamp)
-timestamp = int(time.strftime('%M'))
+timestampm = int(time.strftime('%M'))
 print(timestamp)
-timestamp = int(time.strftime('%S'))
+timestamps = int(time.strftime('%S'))
 print(timestamp)
 # https://docs.python.org/3/library/time.html#time.strftime
 
