ad5c609866319382ab87e9025e4b61813c01a243
katakamX <uni.katakam@gmail.com>
2025-02-16 20:40:10 +0530

    .

---
+++ whileloop.py
@@ -0,0 +1,14 @@
+'''
+print("enetr a number")
+a = int(input())
+
+while a > 0:
+    print("that numebr is greater than 0")
+   ''' 
+   
+i = int(input("enterr the value"))
+while i<38:
+    i = int(input("enetr the number"))
+    print(i)
+    
+print("the num youve entered is greater than 38, ie", i)
