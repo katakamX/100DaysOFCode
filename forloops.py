575fe3be995f696d931048c2ed0a6f01e4498c60
katakamX <uni.katakam@gmail.com>
2025-02-16 20:28:51 +0530

    .

---
+++ forloops.py
@@ -0,0 +1,17 @@
+'''a = "katakam"
+
+for i in reversed(a):
+    if i == "k":
+        print("this is k")
+        
+    print(i, end = ";")
+    
+colors = ["red", "orange", "blue", "yellow", "nigga color"]
+
+for color in reversed(colors):
+    print(color , "\n")
+    for i in color:
+        print(i, end=" \n")'''
+        
+for i in range(0,31,3):
+    print(i)
