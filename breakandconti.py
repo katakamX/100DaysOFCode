d9847f54b1c67b3ed00f2d4efcb789799b462ec5
katakamX <uni.katakam@gmail.com>
2025-03-25 14:37:44 +0530

    .

--- breakandconti.py
+++ breakandconti.py
@@ -1,5 +1,6 @@
 for i in range(0,12):
-    print(" 5 x " , i , " = ", 5*i)
     if(i==10):
         break
-print("came out of loop")
+    print(" 5 x " , i , " = ", 5*i)
+    
+print("came out of lo00000op")
