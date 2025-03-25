7752e0ef9442dd3ae9f378d33a305c27bff80991
katakamX <uni.katakam@gmail.com>
2025-03-25 14:48:04 +0530

    .

--- breakandconti.py
+++ breakandconti.py
@@ -1,6 +1,8 @@
 for i in range(0,12):
     if(i==10):
-        break
+        print("this iteration has been terminated due to foreseeable consequences of the illgal awakening of the 10th iteration of the 5th table.")
+        continue
+    
     print(" 5 x " , i , " = ", 5*i)
     
 print("came out of lo00000op")
---
+++ dowhileUsingWhile.py
@@ -0,0 +1,8 @@
+for i in range(10): 
+     if(i == 9):
+        print("the 9th iteration has been cancelled due it its dormant racial activities")
+        continue
+     print(i)
+    
+         
+    
