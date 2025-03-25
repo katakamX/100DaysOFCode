a9fdde523aa2a60f012b365119e4159f587320a5
katakamX <uni.katakam@gmail.com>
2025-03-25 20:42:08 +0530

    .

--- func.py
+++ func.py
@@ -3,3 +3,11 @@ def gmean(a,b):
     mean = (a*b)/(a+b)
     print(mean)
 gmean(a,b)
+
+def isgreater(a,b):
+    if(a<b):
+        print("a is greater")
+    elif(b>a):
+        print("b is the greater number of the two")
+        
+isgreater(a,b)
