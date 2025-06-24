6658bd514f9fee2f093b2d5281a5248033d73971
katakamX <uni.katakam@gmail.com>
2025-06-24 18:44:25 +0530

    .

--- func.py
+++ func.py
@@ -1,22 +1,10 @@
-a,b=9,10
-def gmean(a,b):
-    mean = (a*b)/(a+b)
-    print(mean)
-gmean(a,b)
+#def average(a=3,b=6):
+ #   print("the average i s", (a+b)/2)
+    
+#average()
 
-'''def isgreater(a,b):
-    if(a<b):
-        print("a is greater")
-    elif(b>a):
-        print("b is the greater number of the two")
-        
-isgreater(a,b)
-
-#def nameprint(fname,mname = "aditya" , lname = "katakam" ):
-    print(fname , mname, lname)
-#nameprint("aditya", "katakam")'''
-
-def average(a,b ,c=1):
-    print("the average is " , (a+b+c)/2)
-
-average(5,6)
+def name(fname="aditya", mname="", lname="katakam"):
+    print("your name is = ", fname,mname,lname)
+    
+name(mname= "putty")
+name("Aditya")
