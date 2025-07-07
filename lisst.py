9729391aa88813e9686746776a4c5aff52ade473
katakamX <uni.katakam@gmail.com>
2025-07-07 20:57:03 +0530

    .

--- lisst.py
+++ lisst.py
@@ -2,3 +2,17 @@ l = [3,5,6]
 print(l)
 print(type(l))
 print(type(l[0]))
+
+asdwda
+l.append(7)
+print(l)            
+l.insert(1, 4)
+print(l)
+l.remove(5)
+print(l)
+
+l.pop(1)
+
+print(l)
+l.pop()
+pri
