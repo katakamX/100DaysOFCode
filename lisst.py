6819ea7c5b631e8866386be0536e2495e6edbe8a
katakamX <uni.katakam@gmail.com>
2025-07-08 15:46:52 +0530

    .

--- lisst.py
+++ lisst.py
@@ -1,5 +1,8 @@
-l = [3,5,6]
+l = [3,5,6,"aditya", 4.5, True]
 print(l)
-print(type(l))
+'''print(type(l))
 print(type(l[0]))
 print(l[0] + l[1])
+print(type(l[3]))'''
+#print(l[-3]) # Negative indexing
+print(l[len(l)-1])
