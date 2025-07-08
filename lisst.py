cd5ba4dd701b7a40b30018f0c0f56d6c7c8b361e
katakamX <uni.katakam@gmail.com>
2025-07-08 19:43:32 +0530

    .

--- lisst.py
+++ lisst.py
@@ -4,5 +4,10 @@ print(l)
 print(type(l[0]))
 print(l[0] + l[1])
 print(type(l[3]))'''
-#print(l[-3]) # Negative indexing
-print(l[len(l)-1])
+# print(l[-3]) # Negative indexing
+print(len(l))
+print(l[len(l)-1]) # Last element
+if "6" in l:
+    print("Yes, '6' is in the list")
+else:
+    print("No, '6' is not in the list")
