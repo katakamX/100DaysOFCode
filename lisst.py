e79833e3c8d97f4251c01b00984a58c0a6381ab0
katakamX <uni.katakam@gmail.com>
2025-07-08 19:46:21 +0530

    .

--- lisst.py
+++ lisst.py
@@ -5,9 +5,11 @@ print(type(l[0]))
 print(l[0] + l[1])
 print(type(l[3]))'''
 # print(l[-3]) # Negative indexing
-print(len(l))
-print(l[len(l)-1]) # Last element
-if "6" in l:
-    print("Yes, '6' is in the list")
-else:
-    print("No, '6' is not in the list")
+# print(len(l))
+# print(l[len(l)-1]) # Last element
+# if "6" in l:
+#     print("Yes, '6' is in the list")
+# else:
+#     print("No", l[2], "is not in the list")
+
+print(l[0:4])
