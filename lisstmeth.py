5d82b92cda8e23d224f9fc80ad24512f4c51c806
katakamX <uni.katakam@gmail.com>
2025-07-14 11:25:39 +0530

    .

---
+++ lisstmeth.py
@@ -0,0 +1,14 @@
+l = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+print(l)
+l.append(16)
+l.insert(0,3)
+# l.reverse()
+print(l)
+# k = l.sort()
+# print(k)
+m = [17, 18, 19, 20]
+l.extend(m)
+print(l)
+l.remove(3)
+print(l)
+print(l.index(10))
